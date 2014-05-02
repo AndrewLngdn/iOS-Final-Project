84736d1919cd98a19f5217031ef307b4c6480ba6
Andrewlngdn <andrewlngdn@gmail.com>
2014-05-01 21:41:28 -0400

    Initial Commit

---
+++ FinalProject/ARLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ARLAppDelegate.h
+//  FinalProject
+//
+//  Created by Andrew Langdon on 5/1/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ARLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FinalProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FinalProject
+//
+//  Created by Andrew Langdon on 5/1/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ARLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ARLAppDelegate class]));
+    }
+}
